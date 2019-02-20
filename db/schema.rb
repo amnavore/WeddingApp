c70264b5991af00baa3b2b6cb176ca8a799bec4d
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-02-20 17:15:31 -0600

    Create Events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_062151) do
+ActiveRecord::Schema.define(version: 2019_02_20_231218) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "date"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "guests", force: :cascade do |t|
     t.string "name"
