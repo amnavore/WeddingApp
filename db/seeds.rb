a8559879cce62f51f8de53cabd1aa8ba8b6bb787
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-15 03:24:24 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@event = Event.create(name:'Event',location:'Place', time:'2019-03-17 12:00:00', description: 'This is an event.')
+@guest = Guest.create(name:'A Name', party:'2', response:'Yes')
