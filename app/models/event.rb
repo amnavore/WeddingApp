e517a8f54dc9dfaa2084dad92c31f7a782756a55
amnavore <abhatti1@hawk.iit.edu>
2019-03-31 14:31:10 -0500

    Create guest_name getter and setter methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,14 @@
 class Event < ApplicationRecord
   has_many :guests
   validates :name, :location, :time, presence: true
-  
+
+  def guest_name=(name)
+  self.guest = Guest.find_or_create_by(name: name)
+end
+
+def guest_name
+  self.guest ? self.guest.name : nil
+end
+
+
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,4 +4,15 @@ class EventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "event responds to guest_name getter" do
+  @event = events(:one)
+  assert_equal @event.guest_name, events(:one).guest.name
+end
+
+test "event responses to guest_name setter" do
+  @event = guests(:one)
+  @event.guest_name = guests(:one).name
+  @event.save
+  assert_equal @event.guest_name, guests(:one).name
+end
 end
