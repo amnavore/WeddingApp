5933b5459419d454289978ce849ed480a982a559
amnavore <abhatti1@hawk.iit.edu>
2019-03-31 14:52:13 -0500

    Remove getter and setter methods for guest_name (for now)

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,13 +2,4 @@ class Event < ApplicationRecord
   has_many :guests
   validates :name, :location, :time, presence: true
 
-  def guest_name=(name)
-  self.guest = Guest.find_or_create_by(name: name)
-end
-
-def guest_name
-  self.guest ? self.guest.name : nil
-end
-
-
 end
