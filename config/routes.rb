fb02117c8427a7a284f271f927ea9a68cccc3d53
amnavore <abhatti1@hawk.iit.edu>
2019-04-01 00:29:22 -0500

    Add guests to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :events, constraints: { id: /\d+/ }
+  resources :events, :guests, constraints: { id: /\d+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
 end
